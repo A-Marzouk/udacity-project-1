255db113088b0898c47f44a7fe5382a747ca033f
perfect_ahmed@yahoo.com <perfect_ahmed@yahoo.com>
2022-07-20 01:11:34 +0300

    #API processing.
    - Update images path. to use path join function.

--- src/controllers/ImagesControllers.ts
+++ src/controllers/ImagesControllers.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import processImageQuery from '../interfaces/images';
 import { fileExists, resizeImageFile } from '../utils/filesUtils';
+import path from 'path';
 
 export const processImage = async (
   req: Request,
@@ -9,8 +10,12 @@ export const processImage = async (
 ): Promise<void> => {
   const { filename, height, width } = req.query as unknown as processImageQuery;
 
-  const filePath = `./src/assets/images/${filename}.jpg`;
-  const resizedFilePath = `./src/assets/thumbs/${filename}_${width}_${height}.jpg`;
+  const filePath = path.join(__dirname, `../../assets/images/${filename}.jpg`);
+
+  const resizedFilePath = path.join(
+    __dirname,
+    `../../assets/thumbs/${filename}_${width}_${height}.jpg`
+  );
 
   const alreadyResized = await fileExists(resizedFilePath);
 
@@ -22,5 +27,5 @@ export const processImage = async (
     }
   }
 
-  return res.status(200).sendFile(resizedFilePath, { root: '.' });
+  return res.status(200).sendFile(resizedFilePath);
 };
--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,13 +1,17 @@
 import supertest from 'supertest';
 import app from '../index';
 import { fileExists } from '../utils/filesUtils';
+import path from 'path';
 
 const request = supertest(app);
 
 describe('Test app functionality', () => {
   it('App can resize an image', async (done) => {
     const filename = 'fjord';
-    const expectedFilePath = `./src/assets/thumbs/${filename}.jpg`;
+    const expectedFilePath = path.join(
+      __dirname,
+      `../../assets/thumbs/${filename}.jpg`
+    );
 
     const response = await request.get(
       `/api/images?filename=${filename}&width=200&height=200`
