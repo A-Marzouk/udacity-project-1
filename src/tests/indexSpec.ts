6cdb62e6cf065ce3a16868a73d73772befc49007
perfect_ahmed@yahoo.com <perfect_ahmed@yahoo.com>
2022-07-20 03:35:49 +0300

    #API processing.
    - Adds test for resize function itself.

--- src/tests/indexSpec.ts
+++ src/tests/indexSpec.ts
@@ -1,12 +1,13 @@
 import supertest from 'supertest';
 import app from '../index';
-import { fileExists } from '../utils/filesUtils';
+import { fileExists, resizeImageFile } from '../utils/filesUtils';
 import path from 'path';
+import { promises as fs } from 'fs';
 
 const request = supertest(app);
 
 describe('Test app functionality', () => {
-  it('App can resize an image', async (done) => {
+  it('App can resize an image through request', async (done) => {
     const filename = 'fjord';
     const expectedFilePath = path.join(
       __dirname,
@@ -22,5 +23,29 @@ describe('Test app functionality', () => {
     expect(fileExists(expectedFilePath)).toBeTruthy();
 
     done();
+  });
+
+  it('Resize function works properly', async () => {
+    const width = '100';
+    const height = '100';
+    const filename = 'fjord';
+
+    const filePath = path.join(
+      __dirname,
+      `../../assets/images/${filename}.jpg`
+    );
+    const fileSize = (await fs.stat(filePath)).size;
+
+    const resizedFilePath = path.join(
+      __dirname,
+      `../../assets/thumbs/${filename}_${width}_${height}.jpg`
+    );
+
+    await resizeImageFile(filePath, resizedFilePath, width, height);
+
+    const resizedFileSize = (await fs.stat(resizedFilePath)).size;
+
+    expect(resizedFileSize).toBeLessThan(fileSize);
+    expect(fileExists(resizedFilePath)).toBeTruthy();
   });
 });
